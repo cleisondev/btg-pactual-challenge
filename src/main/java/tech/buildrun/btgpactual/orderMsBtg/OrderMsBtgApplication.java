786c689e877ca59b835e7a479ce73af2de2692fa
Cleison Nunes <cleison.nunes20@gmail.com>
2025-03-29 12:17:15 -0300

    First commit

---
+++ src/main/java/tech/buildrun/btgpactual/orderMsBtg/OrderMsBtgApplication.java
@@ -0,0 +1,13 @@
+package tech.buildrun.btgpactual.orderMsBtg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderMsBtgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderMsBtgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/buildrun/btgpactual/orderMsBtg/OrderMsBtgApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.buildrun.btgpactual.orderMsBtg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderMsBtgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
